6cfd3024e785ece985d3201cd2e2ddf32aad24fb
Gerard <gerard.dziwinski@infotech.edu.pl>
2025-01-02 13:06:41 +0100

    Zakończenie pracy nad kamerą póki co, kiedyś można dodać camera pan

--- Camera/camera.gd
+++ Camera/camera.gd
@@ -1,15 +1,18 @@
 extends Node3D
 
-var camera_speed:float = 20.0;
+@export_range(0, 100, 1) var camera_speed:float = 20.0;
 
-var camera_zoom_min:float = 10.0;
-var camera_zoom_max:float = 25.0;
+@export_range(0, 100, 1) var camera_zoom_min:float = 10.0;
+@export_range(0, 100, 1) var camera_zoom_max:float = 25.0;
 
-var camera_zoom_speed:float = 30.0;
-
-var camera_zoom_speed_damp:float = 0.92;
+@export_range(0, 100, 1) var camera_zoom_speed:float = 30.0;
+@export_range(0, 2, 0.1) var camera_zoom_speed_damp:float = 0.92;
 
 var camera_zoom_direction:float;
+
+#wszystko z pan jest nieużywane
+@export_range(0, 32, 4) var camera_automatic_pan_margin:int = 16;
+@export_range(0, 20, 0.5) var camera_automatic_pan_speed:float = 12;
 
 @onready var camera:Camera3D = $CameraSocket/Camera3D;
 
@@ -46,3 +49,10 @@ func camera_zoom(delta:float)->void:
 	var new_zoom:float = clamp(camera.position.z + camera_zoom_speed * camera_zoom_direction * delta, camera_zoom_min, camera_zoom_max);
 	camera.position.z = new_zoom
 	camera_zoom_direction *= camera_zoom_speed_damp
+
+func camera_automatic_pan(delta:float)-> void:
+	var viewport_current:Viewport = get_viewport();
+	#[PL] to można kiedyś dokończyć, by gdy myszka zbliża się do krańcy ekranu to kamera się rusza jak, przy WASD
+	#wiem, że nie do końca po polsku, ale no 
+	#[ENG] this can be finished but it's not a priority for now. What it would do is when you 
+	#bring your mouse to the border of the screen the camera would move like with WASD

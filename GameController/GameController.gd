daf65241f7f2dc816ffc725ef2257e8d0d288ca4
Gerard <gerard.dziwinski@infotech.edu.pl>
2025-01-29 22:38:20 +0100

    GameCOntroller single selection działa

--- GameController/GameController.gd
+++ GameController/GameController.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 var SelectedUnits:Array = [];
-@onready var camera = $Camera/CameraSocket/Camera3D;
+var RayLength = 1000
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouse and event.is_action_pressed("Select"):
@@ -9,8 +9,10 @@ func _input(event: InputEvent) -> void:
 			SelectUnit(event.position);
 			
 func SelectUnit(mouse_pos: Vector2):
+	var camera = get_node("Camera/CameraSocket/Camera3D")
+	
 	var ray_orgin = camera.project_ray_origin(mouse_pos)
-	var ray_direction = camera.project_ray_normal(mouse_pos)
+	var ray_direction = ray_orgin + camera.project_ray_normal(mouse_pos) * 1000
 	
 	var space_state = get_world_3d().direct_space_state
 	
@@ -19,8 +21,8 @@ func SelectUnit(mouse_pos: Vector2):
 	
 	var result = space_state.intersect_ray(query)
 	
-	if result and result.collider.is_in_group("Units"):
-		print("batman")
+	if result and result.collider.is_in_group("units"):
+		print("workinguje")
 		clear_selection()
 		result.collider.set_selected(true)
 		SelectedUnits.append(result.collider)
@@ -29,6 +31,9 @@ func clear_selection():
 	for unit in SelectedUnits:
 		unit.set_selected(false)
 	SelectedUnits.clear()
+	
+func _physics_process(delta: float) -> void:
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:

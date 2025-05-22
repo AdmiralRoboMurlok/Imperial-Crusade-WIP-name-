36c331727040973bd468127f8c454de0365c64c5
Gerard <gerard.dziwinski@infotech.edu.pl>
2025-05-22 12:58:47 +0200

    haning myself today live on stream(100% scam no legit)

--- Systems/ResourceSystem.gd
+++ Systems/ResourceSystem.gd
@@ -1,8 +1,14 @@
-extends ResourceSystem 
+extends Node
+class_name ResourceSystem
+
+enum ResourceType {
+	Mine #Add second resouce
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	add_to_group("resources")
+	#collision_layer = 0b00000000_00000000_00000000_00001000
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
